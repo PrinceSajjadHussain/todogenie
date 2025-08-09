93789514d323feec88e427b9bb4a298568db3050
princesajjadhussain <sajjad.aiengineer@gmail.com>
2025-08-10 00:42:46 +0500

    updated

--- supabase/functions/ai-generate-subtasks/index.ts
+++ supabase/functions/ai-generate-subtasks/index.ts
@@ -21,7 +21,8 @@ Deno.serve(async (req: Request) => {
  const allowedOrigins = [
   "http://localhost:8080",
   "https://todogenie-8aqo57vvh-princesajjadhussains-projects.vercel.app",
-  "https://todogenie-git-main-princesajjadhussains-projects.vercel.app/"
+  "https://todogenie-git-main-princesajjadhussains-projects.vercel.app/",
+  "https://todogenie-five.vercel.app/"
 ];
 
 const origin = req.headers.get("Origin") ?? "";
